9767ff46d54fb1bf82a29ee581a989f0cc8d5ad0
Julie Greene <julie_greene22@yahoo.com>
2020-08-26 11:08:28 -0600

    added homepage in package json

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
+import ScrollToTop from "./components/ScrollToTop";
 
 ReactDOM.render(
   <Router>
+    <ScrollToTop>
     <App />
+    </ScrollToTop>
   </Router>,
   document.getElementById('root')
 );
